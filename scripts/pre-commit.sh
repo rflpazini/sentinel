a4a4ca8ba57d5f1eca0d65a5b17c92ede863b569
Rafael Pazini <rflpazini@gmail.com>
2019-01-22 23:33:34 -0200

    fix: test new style guide

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-FILES=$(git diff --cached --name-only --diff-filter=ACM "*.java" | sed 's| |\\ |g')
+FILES=$(git diff --cached --name-only --diff-filter=ACM | paste -s -d",")
 [ -z "$FILES" ] && exit 0
 
 # Prettify all selected files
@@ -9,3 +9,17 @@ echo "$FILES" | xargs ./gradlew goJF -DgoogleJavaFormat.include=\"$FILES\" --dae
 echo "$FILES" | xargs git add
 
 exit 0
+
+
+#
+##!/bin/sh
+##
+## An example hook script to verify proper formatting.
+#
+#files=$(git diff --cached --name-only --diff-filter=ACM | paste -s -d",")
+## this only works if ignoreFailures is set to false (which it is by default)
+#./gradlew verifyGJF -DverifyGoogleJavaFormat.include="$files" &>/dev/null && exit 0
+#
+#echo "Some files are not formatted properly. Please run:"
+#echo "./gradlew gJF -DgoogleJavaFormat.include=\"$files\""
+#exit 1
